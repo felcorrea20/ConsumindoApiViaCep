8d9032a06827de26e0e4629e5c93890078e85769
Felipe Corrêa <felipe.correa2@outlook.com>
2025-11-20 19:51:22 -0300

    Gerando arquivo JSON e melhoria de codigo

--- src/ConsultaCep.java
+++ src/ConsultaCep.java
@@ -1,6 +1,5 @@
 import com.google.gson.Gson;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -18,14 +17,15 @@ public class ConsultaCep {
                 .uri(endereco)
                 .build();
 
-        HttpResponse<String> response = null;
         try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException | IllegalStateException e) {
-            throw new RuntimeException("Não foi possível fazer a busca de endereco do cep fornecido!");
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            return new Gson().fromJson(response.body(), Endereco.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Não foi possível fazer a busca de endereco pelo cep fornecido!");
         }
 
-        return new Gson().fromJson(response.body(), Endereco.class);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,29 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner( System.in );
 
         System.out.print("Digite um CEP para busca( sem traços ): ");
-        var numeroCep = scanner.next();
+        var numeroCep = scanner.nextLine();
 
-        ConsultaCep consultaCep = new ConsultaCep();
-        System.out.println(consultaCep.buscaEndereco(numeroCep));
+        try {
+
+            ConsultaCep consultaCep = new ConsultaCep();
+
+            Endereco endereco = consultaCep.buscaEndereco(numeroCep);
+            System.out.println(endereco);
+
+            GeradorDeArquivo arquivo = new GeradorDeArquivo();
+            arquivo.salvaJson(endereco);
+
+        } catch (RuntimeException | IOException e) {
+
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando aplicação...");
+
+        }
 
     }
 }
